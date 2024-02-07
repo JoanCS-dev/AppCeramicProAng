cdef57cbb3c873ba4ca8eebe2a5238159233875f
CST <cst@MacBook-Air-de-CST.local>
2024-02-06 22:26:08 -0600

    new view

--- ClientApp/src/app/components/private/main/collection-order/collection-order.component.ts
+++ ClientApp/src/app/components/private/main/collection-order/collection-order.component.ts
@@ -6,7 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./collection-order.component.css']
 })
 export class CollectionOrderComponent {
-  
+  public placeholderText: string = "Escribe Aquí";
+  public placeholderPhone: string = "0000 0000 00";
+  public placeholderEmail: string = "ejemplo@gmail.com";
   constructor(){
   }
 }
