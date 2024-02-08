d118e4436d02451eb951f42aa5110dbb88d2dd68
CST <cst@MacBook-Air-de-CST.local>
2024-02-07 20:33:24 -0600

    otrop

--- ClientApp/src/app/components/private/main/index/index.component.ts
+++ ClientApp/src/app/components/private/main/index/index.component.ts
@@ -5,6 +5,9 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './index.component.html'
 })
 export class IndexComponent {
+  public placeholderText: string = "Escribe Aquí";
+  public placeholderPhone: string = "0000 0000 00";
+  public placeholderEmail: string = "ejemplo@gmail.com";
   accountName: string = ""
   constructor(){
     this.accountName = localStorage.getItem("fullName")??"Sin dato";
